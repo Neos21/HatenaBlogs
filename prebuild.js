8a25a11ecaf84b79740f6838c396517d488deb5f
Neos21 <neos21@gmail.com>
2020-11-09 17:40:16 +0900

    Fix

--- prebuild.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-fs.mkdirSync('./dist/styles', { recursive: true });
-fs.mkdirSync('./dist/scripts', { recursive: true });
-
-fs.copyFileSync('./src/scripts/murga.js', './dist/scripts/murga.js');
